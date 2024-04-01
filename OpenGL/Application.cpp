10e5f59a33485b8e96ac12bd9e4ec58ada914688
Hwangsangjin <hwangsangjin88@gmail.com>
2024-04-02 02:59:33 +0900

    GLEW 설정

--- OpenGL/Application.cpp
+++ OpenGL/Application.cpp
@@ -1,4 +1,6 @@
+﻿#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
@@ -19,12 +21,22 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // glfwMakeContextCurrent가 호출된 후에 glewInit이 수행되어야 함
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error\n";
+    }
+
+    // 내 플랫폼의 GL_Version 출력하기
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // 삼각형을 그리는 레거시 코드 추가
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
